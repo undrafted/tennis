1f8ff32146945b2bdca4aa6596ee0abd3e52b5b1
jermiedomingo <8jermiedomingo@gmail.com>
2019-01-19 00:08:30 +0400

    moving the paddle

--- game.js
+++ game.js
@@ -2,9 +2,27 @@ var canvas;
 var canvasContext;
 var ball = {
   x: 50,
-  y: 0,
-  speed: 5
+  y: 50,
+  speedX: 5,
+  speedY: 4
 };
+
+var leftPaddle = {
+  y: 250
+};
+
+const PADDLE_HEIGHT = 100;
+
+function calculateMousePos(evt) {
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top - root.scrollTop;
+  return {
+    x: mouseX,
+    y: mouseY
+  };
+}
 
 window.onload = function() {
   canvas = document.getElementById("gameCanvas");
@@ -12,6 +30,13 @@ window.onload = function() {
 
   var framesPerSecond = 30;
   setInterval(renderGame, 1000 / framesPerSecond);
+
+  canvas.addEventListener("mousemove", function(evt) {
+    var mousePos = calculateMousePos(evt);
+    console.log(mousePos);
+    // set left Paddle Y position (mouse is at the center of paddle)
+    leftPaddle.y = mousePos.y - PADDLE_HEIGHT / 2;
+  });
 };
 
 function renderGame() {
@@ -20,21 +45,26 @@ function renderGame() {
 }
 
 function moveEverything() {
-  ball.x += ball.speed;
+  ball.x += ball.speedX;
+  ball.y += ball.speedY;
 
   if (ball.x > canvas.width || ball.x < 0) {
     // reverse ball direction if it hits the canvas boundary
-    ball.speed = -ball.speed;
+    ball.speedX = -ball.speedX;
+  }
+  if (ball.y > canvas.height || ball.y < 0) {
+    // reverse ball direction if it hits the canvas boundary
+    ball.speedY = -ball.speedY;
   }
 }
 
 function drawEverything() {
   // draw the game area
   colorRect(0, 0, canvas.width, canvas.height, "black");
-  // draw the left pad
-  colorRect(0, 210, 10, 100, "white");
+  // draw the left paddle
+  colorRect(0, leftPaddle.y, 10, PADDLE_HEIGHT, "white");
   // draw the ball
-  colorCircle(ball.x, 150, 10, "white");
+  colorCircle(ball.x, ball.y, 10, "white");
 }
 
 function colorRect(leftX, topY, width, height, drawColor) {
